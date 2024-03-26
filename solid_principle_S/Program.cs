8181128ef7f1af3c74fdc01841233a7c5dadc752
kunta dasharath reddy <156644783+kDasharathreddy@users.noreply.github.com>
2024-03-27 00:06:01 +0530

    Single Responsibility Principle (SRP):
    A class should have only one reason to change, meaning that it should have only one job or responsibility.
    By keeping each class focused on a single responsibility, you can make your code more maintainable and easier to understand.

---
+++ solid_principle_S/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principle_S
+{   
+    class UserHandle
+    {
+        public void AddUser( string username )
+        {
+            Console.WriteLine($"{username} added to the database");
+            
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"{message} logging");
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+    //class Logger
+    //{
+    //    public void Log( string message)
+    //    {
+
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserHandle user = new UserHandle();
+            user.AddUser("dashrath");
+
+
+            UserHandle user2 = new UserHandle();
+            user2.Log("dashrath added succesfullly");
+
+
+
+
+
+
+            //UserHandle user2 = new UserHandle();
+
+
+            // user2.AddUser("")
+
+
+            //Logger loger = new Logger();
+
+
+            ////user.AddUser("dashrath");
+            //loger.Log("user dashrath added succsessfully");
+
+
+        }
+    }
+}
